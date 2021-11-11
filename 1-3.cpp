3ca33559aa7ce4a8a74a4448c10527f4ccb503ed
lypra-ai <81622751+lypra-ai@users.noreply.github.com>
2021-11-11 12:09:54 +0300

    Create 1-3.cpp

---
+++ 1-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits> 
+Using namespace std; 
+Int main() 
+{ 
+    Double c , m , t1, t2 ; 
+    Cout << «input c, m, t1, t2 » ;
+    Cin >> c >> m >> t1 >> t2;
+    Cout <<"Q:"<<pov(t1-t2)*c*m;
+	
+}
