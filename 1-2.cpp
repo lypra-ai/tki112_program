7fcf68a66dd9aeeb523ef13d136a6a1371a9d411
lypra-ai <81622751+lypra-ai@users.noreply.github.com>
2021-11-11 12:09:51 +0300

    Create 1-2.cpp

---
+++ 1-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+using namespace std; 
+
+Int main() 
+{ 
+    double v_boat, v_river, t; 
+    cout << «input v_boat, v_river, t» ;
+    cin >> u >> v >> t;
+    cout <<"s:"<<pov(u+v)*t;
+	
+}
